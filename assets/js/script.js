a0d13299f2db015ea26511d01f80e72224e714c3
Dhruv-232 <118102311+Dhruv-232@users.noreply.github.com>
2023-04-12 16:56:03 +1000

    Finished

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,3 +23,50 @@ searchForm.addEventListener('submit', function (e) {
       cityInput.value = '';
     }
   });
+
+  function getWeather(city) {
+    // construct API URLs for current weather and 5-day forecast
+    const currentWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`;
+    const forecastUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric&appid=${apiKey}`;
+    // get current weather data
+    fetch(currentWeatherUrl)
+      .then((response) => response.json())
+      .then((data) => {
+        // display current weather
+        const currentWeather = `
+          <h2 class="card-title text-center">${data.name}</h2>
+          <p class="text-center">${data.weather[0].description}</p>
+          <p class="text-center">${Math.round(data.main.temp)}&deg;C</p>
+          <p class="text-center">Feels like ${Math.round(data.main.feels_like)}&deg;C</p>
+          <p class="text-center">Humidity: ${data.main.humidity}%</p>
+        `;
+        weatherInfo.innerHTML = currentWeather;
+        // get 5-day forecast data
+        return fetch(forecastUrl);
+      })
+      .then((response) => response.json())
+    .then((data) => {
+      // display 5-day forecast
+      const forecast = data.list.filter((item) => item.dt_txt.includes('12:00:00'));
+      let forecastHtml = '<h3 class="text-center">5-Day Forecast</h3>';
+      forecastHtml += '<div class="row">';
+      forecast.forEach((item) => {
+        const date = new Date(item.dt * 1000);
+        const dayOfWeek = date.toLocaleDateString('en-US', { weekday: 'short' });
+        forecastHtml += `
+          <div class="col">
+            <div class="card text-center">
+              <div class="card-body">
+                <h5 class="card-title">${dayOfWeek}</h5>
+                <img src="https://openweathermap.org/img/wn/${item.weather[0].icon}.png" alt="${item.weather[0].description}">
+                <p>${Math.round(item.main.temp_max)}&deg;C / ${Math.round(item.main.temp_min)}&deg;C</p>
+              </div>
+            </div>
+          </div>
+        `;
+      });
+      forecastHtml += '</div>';
+      weatherInfo.innerHTML += forecastHtml;
+    })
+    .catch((error) => console.log(error));
+}
