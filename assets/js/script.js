ef920ad5e9501f4c1eeaad9ea12ea03f051a647b
Dhruv-232 <118102311+Dhruv-232@users.noreply.github.com>
2023-04-12 16:45:57 +1000

    Worked on the submit button and started doing the getWeather function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,25 @@
 // const apiKey = "053582238c1bd4f0fe68bdd5061c52c1";
 var apiKey = "053582238c1bd4f0fe68bdd5061c52c1";
 
-// get DOM elements
+// getting the DOM elements in javascript
 const searchForm = document.querySelector('form');
 const cityInput = document.getElementById('city-input');
 const weatherInfo = document.getElementById('weather-info');
 const searchHistory = document.getElementById('search-history');
 
+// event listener for search form submission
+searchForm.addEventListener('submit', function (e) {
+    e.preventDefault();
+    const city = cityInput.value.trim();
+    if (city.length > 0) {
+      // add city to search history
+      const li = document.createElement('li');
+      li.classList.add('list-group-item');
+      li.textContent = city;
+      searchHistory.prepend(li);
+      // get current weather and 5-day forecast for the city
+      getWeather(city);
+      // clear input field
+      cityInput.value = '';
+    }
+  });
